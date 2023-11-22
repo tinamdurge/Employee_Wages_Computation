c1564991a17199295dfd88e36bf01083f37c3693
Tinam Durge <97393169+tinamdurge@users.noreply.github.com>
2023-11-22 21:32:30 +0530

    Added: Solved UC1 to check employee is present or absent

---
+++ UC1_Daily_Wage.java
@@ -0,0 +1,12 @@
+public class UC1_Daily_Wage
+{
+public static void main(String[] args)
+{
+int IS_FULL_TIME=1;
+double empcheck = Math.floor(Math.random()*10)%2;
+if(empcheck==IS_FULL_TIME)
+System.out.println("Employee is present");
+else
+System.out.println("Employee is absent");
+}
+}
