4ef2eab6cd28765679dda43afc320ed2d2a20a90
Tinam Durge <97393169+tinamdurge@users.noreply.github.com>
2023-11-22 21:42:47 +0530

    Added: Calculating Wages for a Month

---
+++ UC5_Wage.java
@@ -0,0 +1,32 @@
+public class UC5_Wage
+{
+public static final int IS_PART_TIME=1;
+public static final int IS_FULL_TIME=2;
+public static final int EMP_RATE_PER_HOUR=20;
+public static final int NUM_OF_WORKING_DAYS=30;
+
+public static void main(String[] args)
+{
+int emphrs=0;
+int empwage=0;
+int totalempwage=0;
+for(int day=0;day< NUM_OF_WORKING_DAYS;day++){
+
+int empcheck =(int) Math.floor(Math.random()*10)%3;
+switch (empcheck){
+case IS_PART_TIME:
+emphrs=4;
+break;
+case IS_FULL_TIME:
+emphrs=8;
+break;
+default:
+emphrs=0;
+}
+empwage=emphrs*EMP_RATE_PER_HOUR;
+totalempwage+=empwage;
+System.out.println("Emp Wage: "+empwage);
+}
+System.out.println("Total Emp Wage: "+totalempwage);
+}
+}
